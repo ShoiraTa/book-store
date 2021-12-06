2d57cab9d78eba5c1eb20fb20aa1a26969c894cb
shoiraTa <shoira.shakirovna@gmail.com>
2021-12-06 21:40:55 +0500

    Add router, linter

--- src/index.js
+++ src/index.js
@@ -2,16 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter as Router, Routes, Route, Link} from 'react-router-dom'
+import Categories from './components/Categories'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Router>
+    <header>
+      <Link to='/'> Bookstore</Link>
+      <Link to='/'>Books</Link>
+      <Link to='/categories'>Categories</Link>
+    </header>
+    <Routes>
+      <Route path='/' element={<App/>}/>
+      <Route path='/categories' element = {<Categories/>} />
+    </Routes>
+
+  </Router>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
